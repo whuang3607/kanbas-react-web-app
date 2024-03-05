8d16c5ee2ee7ce29cee0effd3d719e316a6f0f95
whuang3607 <floridian567@gmail.com>
2024-03-05 12:27:17 -0500

    working on HW4 lab

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,12 +1,17 @@
-import {Link} from "react-router-dom";
+import {Link, Navigate, Route, Routes} from "react-router-dom";
 import Assignment3 from "./a3";
 import Nav from "../Nav";
+import Assignment4 from "./a4";
 
 function Labs() {
     return (
         <div>
             <Nav/>
-            <Assignment3/>
+            <Routes>
+                <Route path="/" element={<Navigate to="a3"/>}/>
+                <Route path="a3" element={<Assignment3/>}/>
+                <Route path="a4" element={<Assignment4/>}/>
+            </Routes>
         </div>
     );
  }
