15ddbabd8b2161ea42b65a4cab68b1c138612aa4
whuang3607 <floridian567@gmail.com>
2024-03-09 13:12:30 -0500

    adding images to Dashboard

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
-import { courses } from "../Database";
+import * as db from "../Database";
+
 function Dashboard() {
+  const [courses, setCourses] = useState(db.courses);
+  
+  const [course, setCourse] = useState({
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "valentine_meme.jpg"
+  });
+
+  const addNewCourse = () => {
+    const newCourse = { ...course,
+                        _id: new Date().getTime().toString() };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+
   return (
     <div className="p-4">
-      <h1>Dashboard</h1>              <hr />
-      <h2>Published Courses (7)</h2> <hr />
+      <h1>Dashboard</h1><hr />
+      <h5>Course</h5>
+      <input value={course.name} className="form-control" 
+      onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+      
+      <input value={course.number} className="form-control" 
+      onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+      
+      <input value={course.startDate} className="form-control" type="date" 
+      onChange={(e) => setCourse({ ...course, startDate: e.target.value }) } />
+      
+      <input value={course.endDate} className="form-control" type="date" 
+      onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
+
+      <button onClick={addNewCourse} className="btn btn-primary">
+        Add
+      </button><br/>
+      <br/>
+      <h2>Published Courses ({courses.length})</h2><hr />
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
@@ -14,11 +46,13 @@ function Dashboard() {
                 <img src={`/images/${course.image}`} className="card-img-top" style={{ height: 100 }}/>
                 <div className="card-body">
                   <Link className="card-title" to={`/Kanbas/Courses/${course._id}/Home`}
-                    style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
-                    {course.name} </Link>
-                  <p className="card-text">{course.name}</p>
+                  style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
+                    {course.name}
+                  </Link>
+                  <p className="card-text">{course.number}</p>
                   <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">
-                    Go </Link>
+                    Go
+                  </Link>
                 </div>
               </div>
             </div>
